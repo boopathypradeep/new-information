f364aa02787c6c61633778eb18a6a1e96bbfd22d
boopathypradeep <boopathypradeepr@gmail.com>
2022-05-10 21:10:04 +0530

    updated code

---
+++ src/org/company/Company.java
@@ -0,0 +1,16 @@
+package org.company;
+
+import org.client.Client;
+
+public class Company extends Client {
+	private void companyName() {
+		System.out.println("companyName: ABC");
+
+	}
+	public static void main(String[] args) {
+		Company c=new Company();
+		c.companyName();
+		c.clientName();
+	}
+
+}
